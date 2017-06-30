bd830f5a970ae0a341780a47c9eb7eba78edac3f
Fernando Lima <flasfl@dmz.org.br>
2017-06-30 14:28:02 -0300

    [Fernando] Initial Controls

---
+++ UWPControls/Controls/ExpandedGridView.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace UWPControls.Controls
+{
+    public sealed class ExpandedGridView : GridView
+    {
+        private Grid rootGrid;
+        private const int Row_Height = 130;
+        
+        public string CountItemsHide
+        {
+            get { return (string)GetValue(CountItemsHideProperty); }
+            set { SetValue(CountItemsHideProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Size.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CountItemsHideProperty =
+            DependencyProperty.Register("CountItemsHide", typeof(string), typeof(ExpandedGridView), new PropertyMetadata(string.Empty));
+
+        public ExpandedGridView()
+        {
+            this.DefaultStyleKey = typeof(ExpandedGridView);
+            this.SizeChanged += ExpandedGridView_SizeChanged;
+        }
+        private void ExpandedGridView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var totalItemsVisible = Math.Floor(e.NewSize.Width / Row_Height);
+
+            var totalItemsHide = Items.Count - totalItemsVisible;
+
+            CountItemsHide = "+ " + totalItemsHide.ToString();
+        }
+        private void OpenExpandedToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            rootGrid.RowDefinitions[1].Height = new GridLength(1.0, GridUnitType.Star);
+        }
+        private void OpenExpandedToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            rootGrid.RowDefinitions[1].Height = new GridLength(Row_Height);
+        }
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            ToggleButton OpenExpandedToggleButton = GetTemplateChild<ToggleButton>("OpenExpandedToggleButton");
+            rootGrid = GetTemplateChild<Grid>("RootGrid");
+
+            OpenExpandedToggleButton.Checked += OpenExpandedToggleButton_Checked;
+            OpenExpandedToggleButton.Unchecked += OpenExpandedToggleButton_Unchecked;
+        }
+        private T GetTemplateChild<T>(string name) where T : class
+        {
+            object obj = GetTemplateChild(name);
+
+            return ((obj != null && obj is T) ? (T)obj : null);
+        }
+    }
+}
---
+++ UWPControls/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPControls
+{
+
+    public class Media
+    {
+        public BitmapImage Image { get; set; }
+    }
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<Media> Items { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Items = new ObservableCollection<Media>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                Items.Add(new Media { Image = new BitmapImage(new Uri(@"ms-appx:///Assets/photo.jpg")) });
+            }
+
+
+        }
+    }
+}
